c47cc5eece538e778db2941646a68683aba38465
Ataur214 <ataur.214@gmail.com>
2023-09-03 10:41:59 +0600

    modify visualTesting

--- tests/visualTesting.spec.js
+++ tests/visualTesting.spec.js
@@ -1,9 +1,9 @@
 const {test, expect} = require('@playwright/test');
 
-test('open the browser', async ({browser})=>{
+test('Visual Testing - Compare the two image to find the difference', async ({browser})=>{
     const context = await browser.newContext();
     const page = await context.newPage();
-    await page.goto('https://rahulshettyacademy.com');
+    await page.goto('https://rahulshettyacademy.com/locatorspractice/');
     console.log(await page.title());
     expect(await page.screenshot()).toMatchSnapshot('homepage.png');
 });
