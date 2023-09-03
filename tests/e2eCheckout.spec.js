fc22fc5ca0249a6670b716262011d994f39cb45e
Ataur214 <ataur.214@gmail.com>
2023-09-03 19:49:53 +0600

    Read test data from external JSON file
    Drive the test data from external JSON file

--- tests/e2eCheckout.spec.js
+++ tests/e2eCheckout.spec.js
@@ -4,35 +4,31 @@ const {ProductPage} = require('../pageObject/productPage');
 const { CartPage } = require('../pageObject/cartPage');
 const { CheckoutPage } = require('../pageObject/checkOutPage');
 const { MyOrderPage } = require('../pageObject/myOrderPage');
+const data = JSON.parse(JSON.stringify(require('../resources/userdetails.json')));
 
 test('E2E test for product checkout ', async ({ page }) => {
-    const userEmail = 'noiceataure@gmail.com';
-    const userPassword = '@Ata1234';
-    const productName = 'adidas original';
-    const Cvv = '123';
-    const cardName = 'Ataure';
-    const CountryName = 'ind';
+    
     const successMessage = page.locator('.hero-primary');
    
     const loginPage = new LoginPage(page,expect);
     await loginPage.goTo();
     await expect(page).toHaveTitle("Let's Shop");
 
-    await loginPage.validLogin(userEmail,userPassword);
+    await loginPage.validLogin(data.userEmail,data.userPassword);
    
     //add product to my shopping cart
     const productPage = new ProductPage(page)
-    await productPage.addToCard(productName);
+    await productPage.addToCard(data.productName);
 
     //Assert the card details dynamically
     const cartpage = new CartPage(page);
     await cartpage.navigateToMyCart();
-    await cartpage.checkMyCartDetails(productName);
+    await cartpage.checkMyCartDetails(data.productName);
 
     //Fill up the order details
     const checkoutpage = new CheckoutPage(page);
     await checkoutpage.navigateToCheckOutDetails();
-    await checkoutpage.inputDetailsToCheckOut(Cvv,cardName,CountryName,userEmail);
+    await checkoutpage.inputDetailsToCheckOut(data.Cvv,data.cardName,data.CountryName,data.userEmail);
     await checkoutpage.clickPlaceOrderButton();
     
     // after place order assert some cases
